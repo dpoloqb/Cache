c7510497e958a043b3a8b85fd0f4d5f5e0932f86
dpoloqb <ecleric123@mail.ru>
2025-09-22 21:32:05 +0300

    splited lookup_update on 4 functions; created new function isFull, that replaced full_Am, full_A1in and full_A1out; removed the colors.hpp library for now

--- include/cache.hpp
+++ include/cache.hpp
@@ -7,7 +7,8 @@
 namespace caches
 {
 
-    template <typename T, typename KeyT = int> struct cache_t
+    template <typename T, typename KeyT = int>
+    struct TwoQueuesCache
     {
     
     private:
@@ -27,25 +28,13 @@ namespace caches
         std::unordered_map<KeyT, ListIt> hash_Am;
         std::unordered_map<KeyT, ListIt> hash_A1in;
         std::unordered_map<KeyT, A1outIt> hash_A1out;
-
-        bool full_Am() const { return (cache_Am.size() == Am_size_); }
-        bool full_A1in() const { return (cache_A1in.size() == A1in_size_); }
-        bool full_A1out() const { return (cache_A1out.size() == A1out_size_); } 
-
-    public:
-
-        cache_t(size_t cacheSize)
-        {
-            cacheSize_ = cacheSize;
-            Am_size_ = cacheSize / 2;
-            A1in_size_ = cacheSize / 2;
-            A1out_size_ = 2 * cacheSize;
-        }
         
+        template <typename QTy>
+        bool isFull(QTy& Q, size_t refSize) const
+        { return (Q.size() == refSize); }
 
-        template <typename F> bool lookup_update(KeyT key, F slow_get_page)
+        bool check_Am(KeyT key)
         {
-            //std::cout << cacheSize_;
             auto hit_Am = hash_Am.find(key);
             
             if (hit_Am != hash_Am.end())
@@ -53,17 +42,21 @@ namespace caches
 
                 auto eltit_Am = hit_Am->second;
                 cache_Am.splice(cache_Am.begin(), cache_Am, eltit_Am);
-                //FIXME - может нужно что-то менять в хэш-таблице
 
                 return true;
             }
 
+            return false;
+        }
+
+        bool check_A1in(KeyT key)
+        {
             auto hit_A1in = hash_A1in.find(key);
 
             if (hit_A1in != hash_A1in.end())
             {
 
-                if (full_Am()) //TODO - обобщить удаление из списка и хэш таблицы
+                if (isFull(cache_Am, Am_size_))
                 {
                     hash_Am.erase(cache_Am.back().first); 
                     cache_Am.pop_back();
@@ -76,11 +69,17 @@ namespace caches
                 return true;
             }
 
+            return false;
+        }
+
+        template <typename F>
+        bool check_A1out(KeyT key, F slow_get_page)
+        {
             auto hit_A1out = hash_A1out.find(key);
 
             if (hit_A1out != hash_A1out.end()) 
             {
-                if (full_Am()) //TODO - обобщить удаление из списка и хэш таблицы
+                if (isFull(cache_Am, Am_size_))
                 {
                     hash_Am.erase(cache_Am.back().first); 
                     cache_Am.pop_back();
@@ -89,13 +88,17 @@ namespace caches
                 hash_Am.emplace(key, cache_Am.begin());
                 hash_A1out.erase(hit_A1out->first);
                 cache_A1out.erase(hit_A1out->second);
-
                 return false;
             }
+            return true;
+        }
 
-            if (full_A1in())
+        template <typename F>
+        void get_new_elem(KeyT key, F slow_get_page)
+        {
+            if (isFull(cache_A1in, A1in_size_))
             {
-                if (full_A1out())
+                if (isFull(cache_A1out, A1out_size_))
                 {
                     hash_A1out.erase(cache_A1out.back());
                     cache_A1out.pop_back();
@@ -107,10 +110,25 @@ namespace caches
             }
             cache_A1in.emplace_front(key, slow_get_page(key)); 
             hash_A1in.emplace(key, cache_A1in.begin());
-
-            return false;
         }
 
-    };
+    public:
 
+        TwoQueuesCache(size_t cacheSize) : cacheSize_(cacheSize), Am_size_(cacheSize / 2),
+                                    A1in_size_(cacheSize / 2), A1out_size_(cacheSize * 2) {}
+
+        template <typename F>
+        bool lookup_update(KeyT key, F slow_get_page)
+        {
+            if (check_Am(key))
+                return true;
+            if (check_A1in(key))
+                return true;
+            if (!check_A1out(key, slow_get_page))
+                return false;
+
+            get_new_elem(key, slow_get_page);
+            return false;
+        }
+    };
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
-#include "include/colors.hpp"
 #include "include/cache.hpp"
 
 int slow_get_page_int(int key) { return key; }
@@ -14,7 +13,7 @@ int main ()
 
     std::cin >> cacheSize >> nItems;
     assert(std::cin.good());
-    caches::cache_t<int> c{cacheSize};
+    caches::TwoQueuesCache<int> c{cacheSize};
 
     for (int i = 0; i < nItems; ++i)
     {
@@ -25,25 +24,5 @@ int main ()
             hits += 1;
     }
     std::cout << hits << std::endl;
-    #if 0
-    std::cout << GREEN << "Please enter the cache size:" << RESET << std::endl;
 
-    size_t cacheSize = 0;
-    std::cin >> cacheSize;
-
-    std::cout << GREEN << "Please enter the number of items:" << RESET << std::endl;
-
-    size_t nItems = 0;
-    std::cin >> nItems;
-
-    std::vector<int> vec = {};
-
-    std::cout << GREEN << "Please enter " << nItems << " items:" << RESET << std::endl;
-    int value = 0;
-    for (size_t i = 0; i < nItems; i++)
-    {
-        std::cin >> value;
-        vec.push_back (value);
-    }
-    #endif
 }
